a332fb1ea39c9466cf1ca4d6ef40469e4c89406b
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-07-09 10:10:13 -0300

    feat: add PostgreSQL setup with Sequelize integration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
 import express from 'express';
 import router from './shared/interface/routes';
+import { Sequelize } from 'sequelize';
+
 const app = express();
 
 app.use(router);
+
+const sequelize = new Sequelize('protos-farm', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres', // or 'postgres', 'sqlite', etc.
+  port: 5433, // Change to the port you mapped in docker-compose
+});
+
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+    console.log('🟢 Database connected');
+  } catch (error) {
+    console.error('🔴 Failed to connect to the database:', error);
+  }
+}
+startServer();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
