ca0b3221745ab967b771f4768bc7658e495f3b9a
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-07-09 12:01:56 -0300

    feat: add dotenv for environment variable management and update server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
 import express from 'express';
 import router from './shared/interface/routes';
+import 'dotenv/config'; // Load environment variables from .env file
 import { Sequelize } from 'sequelize';
 
 const app = express();
 
 app.use(router);
 
-const sequelize = new Sequelize('protos-farm', 'postgres', 'postgres', {
+const POSTGRES_DB = process.env.POSTGRES_DB;
+if (!POSTGRES_DB) {
+  throw new Error('Environment variable POSTGRES_DB is not set');
+}
+
+const POSTGRES_USER = process.env.POSTGRES_USER;
+if (!POSTGRES_USER) {
+  throw new Error('Environment variable POSTGRES_USER is not set');
+}
+
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+if (!POSTGRES_PASSWORD) {
+  throw new Error('Environment variable POSTGRES_PASSWORD is not set');
+}
+
+const sequelize = new Sequelize(POSTGRES_DB, 'postgres', 'postgres', {
   host: 'localhost',
   dialect: 'postgres', // or 'postgres', 'sqlite', etc.
   port: 5433, // Change to the port you mapped in docker-compose
