a304ba3e0d9405ba0c6530373ad866efb29aadea
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-07-10 15:49:01 -0300

    feat: update .gitignore to include .vercel and clean up server.ts code

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,35 @@
+import 'dotenv/config'; // Load environment variables from .env file
 import express from 'express';
 import router from './shared/interface/routes';
-import 'dotenv/config'; // Load environment variables from .env file
-import { Sequelize, type Dialect } from 'sequelize';
-import dbEnvironmentVariables from './shared/interface/config';
 
 const app = express();
 
 app.use(router);
 
-const sequelize = new Sequelize(
-  dbEnvironmentVariables.POSTGRES_DB,
-  dbEnvironmentVariables.POSTGRES_USER,
-  dbEnvironmentVariables.POSTGRES_PASSWORD,
-  {
-    host: dbEnvironmentVariables.POSTGRES_HOST,
-    dialect: dbEnvironmentVariables.POSTGRES_DIALECT as Dialect,
-    port: Number(dbEnvironmentVariables.POSTGRES_PORT), // Change to the port you mapped in docker-compose
-  },
-);
+app.get('/', (req, res) => {
+  res.send('Welcome to Protos Farm API');
+});
 
-async function startServer() {
-  try {
-    await sequelize.authenticate();
-    console.log('🟢 Database connected');
-  } catch (error) {
-    console.error('🔴 Failed to connect to the database:', error);
-  }
-}
-startServer();
+// const sequelize = new Sequelize(
+//   dbEnvironmentVariables.POSTGRES_DB,
+//   dbEnvironmentVariables.POSTGRES_USER,
+//   dbEnvironmentVariables.POSTGRES_PASSWORD,
+//   {
+//     host: dbEnvironmentVariables.POSTGRES_HOST,
+//     dialect: dbEnvironmentVariables.POSTGRES_DIALECT as Dialect,
+//     port: Number(dbEnvironmentVariables.POSTGRES_PORT), // Change to the port you mapped in docker-compose
+//   },
+// );
+
+// async function startServer() {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('🟢 Database connected');
+//   } catch (error) {
+//     console.error('🔴 Failed to connect to the database:', error);
+//   }
+// }
+// startServer();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
